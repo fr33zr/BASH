fef07391e93c547496afc170587d7ecb87fc3c8d
opprssr <47411492+fr33zr@users.noreply.github.com>
2019-11-18 12:53:04 -0800

    Update disip6.sh
    Re-enable lines just in case.

--- disip6.sh
+++ disip6.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
+
+# Comment the above two lines and uncomment the bottom two re-enable IPV6
+#  sysctl -w net.ipv6.conf.all.disable_ipv6=0
+#  sysctl -w net.ipv6.conf.default.disable_ipv6=0
+
